a1314017a1dfa24afd6a658722e05ce3066ed58c
h0p3zZ <danielwimmer08012003@gmail.com>
2020-05-29 13:33:04 +0200

    Updated Config with URLs

--- PosProject/app/src/main/java/com/htlgrieskirchen/posproject/Config.java
+++ PosProject/app/src/main/java/com/htlgrieskirchen/posproject/Config.java
@@ -10,5 +10,14 @@ public class Config {
     public static int RC_SETTINGS = 1000;
 
     //server URL
-    public static String SERVER_URL = "";
+    public static String SERVER_URL = "http://85.118.186.133:8080/";
+    //URL for reservations
+    public static String RESERVATIONS_URL1 = "reservations/getReservationsForTable?restaurantNumber=";
+    public static String RESERVATIONS_URL2 = "&tableNumber=1";
+    //URL for reservationCodes
+    public static String RESERVATION_CODE_URL = "http://85.118.186.133:8080/restaurants/getdbid?name=";
+    //URL for nearest restaurants
+    public static String RESTAURANT_URL1 = "http://85.118.186.133:8080/restaurants/getTenNearestRestaurants?lon=";
+    public static String RESTAURANT_URL2 = "&lat=";
+    public static String RESTAURANT_URL_DISTANCE = "&distance"; //distance in metres
 }
