ab1e25204d8f28235d1c08dc6ad0c6442e096c0d
H0-pe <leon.sterner@gmx.at>
2020-05-29 14:30:01 +0200

    Architecture Changed

---
+++ api/src/main/java/at/htlgrieskirchen/sternerwimmer/api/ReservationRestController.java
@@ -0,0 +1,32 @@
+package at.htlgrieskirchen.sternerwimmer.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/reservations")
+public class ReservationRestController {
+    @Autowired
+    RestaurantRepository restaurantRepository;
+
+    @GetMapping("/getReservationsForTable")
+    public List<Reservation> getReservationsForTable(@RequestParam(value = "restaurantNumber") String restaurantNumber, @RequestParam(value = "tableNumber") String tableNumber) {
+        return restaurantRepository.findByRestaurantNumber(restaurantNumber).getTables().stream().filter(table -> table.getId().equals(tableNumber)).findFirst().get().getReservations();
+    }
+
+    @PatchMapping("/addReservation")
+    //@RequestParam(value = "restaurantNumber")String restaurantNumber, @RequestParam(value = "tableNumber") String tableNumber,
+    public void addReservation(@RequestBody Reservation reservation){
+        List<Reservation> reservations = restaurantRepository.findByRestaurantNumber(reservation.getRestaurantNumber()).getTables().stream()
+                .filter(table -> table.getId().equals(reservation.getTableNumber()))
+                .findFirst().get().getReservations();
+       reservations.add(reservation);
+    }
+
+}
+
