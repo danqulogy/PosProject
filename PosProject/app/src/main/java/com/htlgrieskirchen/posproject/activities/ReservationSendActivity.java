c219e1df13457978d14ef85c0daea5493d11473c
H0-pe <leon.sterner@gmx.at>
2020-06-19 18:01:35 +0200

    Removed CalendarView; Implemented DatePickerDialog to chose date of reservation

--- PosProject/app/src/main/java/com/htlgrieskirchen/posproject/activities/ReservationSendActivity.java
+++ PosProject/app/src/main/java/com/htlgrieskirchen/posproject/activities/ReservationSendActivity.java
@@ -2,9 +2,15 @@ package com.htlgrieskirchen.posproject.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,26 +19,39 @@ import com.htlgrieskirchen.posproject.beans.Restaurant;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ReservationSendActivity extends AppCompatActivity {
 
     Restaurant restaurant;
-    CalendarView calendarView;
-    LocalDate localDate;
-
-
+    Button dateButton;
+    EditText date;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation_send);
-        calendarView = (CalendarView) findViewById(R.id.reservation_send_calendar);
+        this.context = this;
         restaurant = getIntent().getParcelableExtra("restaurant");
 
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                //MONTH FROM 0-11
-                localDate = LocalDate.of(year, month+1, dayOfMonth);
+        dateButton = findViewById(R.id.reservation_send_date_button);
+        date = findViewById(R.id.reservation_send_date);
+
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              DatePickerDialog datePickerDialog = new DatePickerDialog(context);
+              datePickerDialog.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        date.setText(LocalDate.of(year,month+1,dayOfMonth).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+                    }
+                });
+              datePickerDialog.show();
             }
         });
+
+
     }
 }
